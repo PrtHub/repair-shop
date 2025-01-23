3ae63cf8c276f3b580c95bb48c7037e8e7e66a4a
Pritam <pri10145@gmail.com>
2025-01-23 21:10:53 +0530

    type safe form inputs created

--- app/(rs)/customers/form/customer-form.tsx
+++ app/(rs)/customers/form/customer-form.tsx
@@ -1,63 +1,133 @@
-"use client"
+"use client";
 
-import { zodResolver } from "@hookform/resolvers/zod"
-import { useForm } from "react-hook-form"
-import { Form } from "@/components/ui/form"
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { Form } from "@/components/ui/form";
 
 import {
-    insertCustomerSchema,
-    InsertCustomerSchemaType,
-    SelectCustomerSchemaType
-} from "@/lib/zod-schema/customer"
+  insertCustomerSchema,
+  InsertCustomerSchemaType,
+  SelectCustomerSchemaType,
+} from "@/lib/zod-schema/customer";
+import { InputWithLabel } from "@/components/inputs/InputWithLabel";
+import { Button } from "@/components/ui/button";
+import { SelectWithLabel } from "@/components/inputs/SelectWithLabel";
+import { TextAreaWithLabel } from "@/components/inputs/TextAreaWithLabel";
+import { StatesArray } from "@/constants/states";
 
 type Props = {
-    customer?: SelectCustomerSchemaType
-}
+  customer?: SelectCustomerSchemaType;
+};
 
-const CustomerForm = ({
-    customer
-}: Props) => {
+const CustomerForm = ({ customer }: Props) => {
+  const defaultValues = {
+    id: customer?.id || 0,
+    firstName: customer?.firstName || "",
+    lastName: customer?.lastName || "",
+    address1: customer?.address1 || "",
+    address2: customer?.address2 || "",
+    city: customer?.city || "",
+    state: customer?.state || "",
+    email: customer?.email || "",
+    zip: customer?.zip || "",
+    phone: customer?.phone || "",
+    notes: customer?.notes || "",
+  };
 
-    const form = useForm<InsertCustomerSchemaType>({
-        mode: "onBlur",
-        resolver: zodResolver(insertCustomerSchema),
-        defaultValues: {
-            id: customer?.id || 0,
-            firstName: customer?.firstName || "",
-            lastName: customer?.lastName || "",
-            address1: customer?.address1 || "",
-            address2: customer?.address2 || "",
-            city: customer?.city || "",
-            state: customer?.state || "",
-            email: customer?.email || "",
-            zip: customer?.zip || "",
-            phone: customer?.phone || "",
-            notes: customer?.notes || "",
-        }
-    })
+  const form = useForm<InsertCustomerSchemaType>({
+    mode: "onBlur",
+    resolver: zodResolver(insertCustomerSchema),
+    defaultValues,
+  });
 
-    const onSubmit = (data: InsertCustomerSchemaType) => {
-        console.log(data)
-    }
+  const onSubmit = (data: InsertCustomerSchemaType) => {
+    console.log(data);
+  };
 
-    return (
-        <div className="flex flex-col gap-1 sm:px-8">
-            <div>
-                <h2 className="text-2xl font-bold">
-                    {customer?.id ? "Edit" : "New"} Customer Form
-                </h2>
+  const handleReset = () => {
+    form.reset(defaultValues);
+  };
+
+  return (
+    <div className="flex flex-col gap-1 sm:px-8 py-4">
+      <div>
+        <h2 className="text-2xl font-bold">
+          {customer?.id ? "Edit" : "New"} Customer Form
+        </h2>
+      </div>
+      <Form {...form}>
+        <form
+          onSubmit={form.handleSubmit(onSubmit)}
+          className="flex flex-col md:flex-row gap-4 md:gap-8 mt-8"
+        >
+          <div className="flex flex-col gap-4 w-full max-w-xs">
+            <InputWithLabel<InsertCustomerSchemaType>
+              fieldTitle="First Name"
+              nameInSchema="firstName"
+            />
+            <InputWithLabel<InsertCustomerSchemaType>
+              fieldTitle="Last Name"
+              nameInSchema="lastName"
+            />
+            <InputWithLabel<InsertCustomerSchemaType>
+              fieldTitle="Address 1"
+              nameInSchema="address1"
+            />
+            <InputWithLabel<InsertCustomerSchemaType>
+              fieldTitle="Address 2"
+              nameInSchema="address2"
+            />
+            <InputWithLabel<InsertCustomerSchemaType>
+              fieldTitle="City"
+              nameInSchema="city"
+            />
+            <SelectWithLabel<InsertCustomerSchemaType>
+              fieldTitle="State"
+              nameInSchema="state"
+              data={StatesArray}
+            />
+          </div>
+          <div className="flex flex-col gap-4 w-full max-w-xs">
+            <InputWithLabel<InsertCustomerSchemaType>
+              fieldTitle="Zip Code"
+              nameInSchema="zip"
+            />
+            <InputWithLabel<InsertCustomerSchemaType>
+              fieldTitle="Email"
+              nameInSchema="email"
+            />
+            <InputWithLabel<InsertCustomerSchemaType>
+              fieldTitle="Phone"
+              nameInSchema="phone"
+            />
+            <TextAreaWithLabel<InsertCustomerSchemaType>
+              fieldTitle="Notes"
+              nameInSchema="notes"
+              className="h-20"
+            />
+            <div className="flex gap-2">
+              <Button
+                type="submit"
+                className="w-3/4"
+                variant="default"
+                title="Save"
+              >
+                Save
+              </Button>
+              <Button
+                type="button"
+                variant="destructive"
+                title="Reset"
+                onClick={handleReset}
+              >
+                Reset
+              </Button>
             </div>
-            <Form {...form}>
-                <form
-                    onSubmit={form.handleSubmit(onSubmit)}
-                    className="flex flex-col sm:flex-row gap-4 sm:gap-8"
-                >
-                    <p>{JSON.stringify(form.getValues())}</p>
-                </form>
-            </Form>
+          </div>
+        </form>
+      </Form>
+    </div>
+  );
+};
 
-        </div>
-    )
-}
-
-export default CustomerForm
+export default CustomerForm;
--- app/(rs)/tickets/form/ticket-form.tsx
+++ app/(rs)/tickets/form/ticket-form.tsx
@@ -5,59 +5,124 @@ import { zodResolver } from "@hookform/resolvers/zod";
 
 import { Form } from "@/components/ui/form";
 
-import { 
-    insertTicketSchema, 
-    InsertTicketSchemaType, 
-    SelectTicketSchemaType 
+import {
+  insertTicketSchema,
+  InsertTicketSchemaType,
+  SelectTicketSchemaType,
 } from "@/lib/zod-schema/ticket";
 import { SelectCustomerSchemaType } from "@/lib/zod-schema/customer";
+import { Button } from "@/components/ui/button";
+import { InputWithLabel } from "@/components/inputs/InputWithLabel";
+import { CheckboxWithLabel } from "@/components/inputs/CheckboxWithLabel";
+import { TextAreaWithLabel } from "@/components/inputs/TextAreaWithLabel";
 
 type Props = {
-    ticket?: SelectTicketSchemaType
-    customer?: SelectCustomerSchemaType
-}
+  ticket?: SelectTicketSchemaType;
+  customer?: SelectCustomerSchemaType;
+};
 
-const TicketForm = ({
-    ticket,
-    customer
-}: Props) => {
+const TicketForm = ({ ticket, customer }: Props) => {
+  const defaultValues = {
+    id: ticket?.id || 0,
+    customerId: ticket?.customerId || customer?.id || 0,
+    title: ticket?.title || "",
+    description: ticket?.description || "",
+    tech: ticket?.tech ?? "new-ticket@example.com",
+    completed: ticket?.completed ?? false,
+  };
 
-    const form = useForm<InsertTicketSchemaType>({
-        mode: "onBlur",
-        resolver: zodResolver(insertTicketSchema),
-        defaultValues: {
-            id: ticket?.id || 0,
-            customerId: ticket?.customerId || customer?.id || 0,
-            title: ticket?.title || "",
-            description: ticket?.description || "",
-            tech: ticket?.tech ?? "new-ticket@example.com",
-            completed: ticket?.completed ?? false
-        }
-    });
+  const form = useForm<InsertTicketSchemaType>({
+    mode: "onBlur",
+    resolver: zodResolver(insertTicketSchema),
+    defaultValues,
+  });
 
+  const onSubmit = (data: InsertTicketSchemaType) => {
+    console.log(data);
+  };
 
-    const onSubmit = (data: InsertTicketSchemaType) => {
-        console.log(data)
-    }
+  const handleReset = () => {
+    form.reset(defaultValues);
+  };
 
-    return (
-        <div className="flex flex-col gap-1 sm:px-8">
-        <div>
-            <h2 className="text-2xl font-bold">
-             {ticket?.id ? "Edit" : "New"} Ticket {ticket?.id ? `#${ticket.id}` : "Form"}
-            </h2>
-        </div>
-        <Form {...form}>
-            <form
-                onSubmit={form.handleSubmit(onSubmit)}
-                className="flex flex-col sm:flex-row gap-4 sm:gap-8"
-            >
-                <p>{JSON.stringify(form.getValues())}</p>
-            </form>
-        </Form>
+  return (
+    <div className="flex flex-col gap-1 sm:px-8 py-4">
+      <div>
+        <h2 className="text-2xl font-bold">
+          {ticket?.id ? "Edit" : "New"} Ticket{" "}
+          {ticket?.id ? `#${ticket.id}` : "Form"}
+        </h2>
+      </div>
+      <Form {...form}>
+        <form
+          onSubmit={form.handleSubmit(onSubmit)}
+          className="flex flex-col md:flex-row gap-4 md:gap-8 mt-8"
+        >
+          <div className="flex flex-col gap-4 w-full max-w-xs">
+            <InputWithLabel<InsertTicketSchemaType>
+              fieldTitle="Title"
+              nameInSchema="title"
+            />
 
+            <InputWithLabel<InsertTicketSchemaType>
+              fieldTitle="Tech"
+              nameInSchema="tech"
+              disabled={true}
+            />
+
+            <CheckboxWithLabel<InsertTicketSchemaType>
+              fieldTitle="Completed"
+              nameInSchema="completed"
+              message="Yes"
+            />
+            <TextAreaWithLabel<InsertTicketSchemaType>
+              fieldTitle="Description"
+              nameInSchema="description"
+              className="h-40"
+            />
+
+            <div className="flex gap-2">
+              <Button
+                type="submit"
+                className="w-3/4"
+                variant="default"
+                title="Save"
+              >
+                Save
+              </Button>
+
+              <Button
+                type="button"
+                variant="destructive"
+                title="Reset"
+                onClick={handleReset}
+              >
+                Reset
+              </Button>
+            </div>
+          </div>
+
+          <div className="flex flex-col gap-4 w-full max-w-xs">
+            <div className="mt-4 space-y-2">
+              <h3 className="text-lg font-semibold">Customer Info</h3>
+              <hr className="w-4/5" />
+              <p>
+                {customer?.firstName} {customer?.lastName}
+              </p>
+              <p>{customer?.address1}</p>
+              {customer?.address2 ? <p>{customer?.address2}</p> : null}
+              <p>
+                {customer?.city}, {customer?.state} {customer?.zip}
+              </p>
+              <hr className="w-4/5" />
+              <p>{customer?.email}</p>
+              <p>Phone: {customer?.phone}</p>
+            </div>
+          </div>
+        </form>
+      </Form>
     </div>
-    )
+  );
 };
 
 export default TicketForm;
